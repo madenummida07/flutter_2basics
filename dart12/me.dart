11f5e72a2737a96f3b26359dd18f13eee69526d3
Ummida Maden <abdurahim.amirovv@gmail.com>
2024-04-24 12:53:32 +0600

    added me file

---
+++ dart12/me.dart
@@ -0,0 +1,26 @@
+void main() {
+  hello();
+  printName();
+  documentation();
+}
+
+/// this is hello method, do yourself
+void hello() {
+  print("Hello");
+}
+
+/// methods for printing name
+void printName() {
+  print("My name is Ummida, I am from Taraz");
+}
+
+/**
+* this type of comment for documentation
+*/
+void documentation() {
+  print("""
+Date of Birth: January 4, 2007
+Contact Information: madenummida@email.com | 8 771 158 89 39
+Occupation/Profession: IT Programmist
+""");
+}
